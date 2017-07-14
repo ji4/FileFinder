3fd79210479309a0d7be3318a4adbee6f109aaa2
ji4 <bornofluv@gmail.com>
2017-07-14 10:26:13 +0800

    Add EditText & Button in MainActivity

--- app/src/main/java/com/javatechig/listallfiles/MainActivity.java
+++ app/src/main/java/com/javatechig/listallfiles/MainActivity.java
@@ -23,53 +23,53 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		view = (LinearLayout) findViewById(R.id.view);
-
-		//getting SDcard root path
-		root = new File(Environment.getExternalStorageDirectory()
-				.getAbsolutePath());
-		getfile(root);
-
-		for (int i = 0; i < fileList.size(); i++) {
-			TextView textView = new TextView(this);
-			textView.setText(fileList.get(i).getName());
-			textView.setPadding(5, 5, 5, 5);
-
-			System.out.println(fileList.get(i).getName());
-
-//			if (fileList.get(i).isDirectory()) {
-//				textView.setTextColor(Color.parseColor("#FF0000"));
-//			}
-
-			if (fileList.get(i).isFile())
-				view.addView(textView);
-		}
-
-	}
-
-	public ArrayList<File> getfile(File dir) {
-		File listFile[] = dir.listFiles();
-		if (listFile != null && listFile.length > 0) {
-			for (int i = 0; i < listFile.length; i++) {
-
-				if (listFile[i].isDirectory()) {
-					fileList.add(listFile[i]);
-					getfile(listFile[i]);
-
-				} else {
-//					if (listFile[i].getName().endsWith(".png")
-//							|| listFile[i].getName().endsWith(".jpg")
-//							|| listFile[i].getName().endsWith(".jpeg")
-//							|| listFile[i].getName().endsWith(".gif"))
+//		view = (LinearLayout) findViewById(R.id.view);
 //
-//					{
-						fileList.add(listFile[i]);
-//					}
-				}
-
-			}
-		}
-		return fileList;
+//		//getting SDcard root path
+//		root = new File(Environment.getExternalStorageDirectory()
+//				.getAbsolutePath());
+//		getfile(root);
+//
+//		for (int i = 0; i < fileList.size(); i++) {
+//			TextView textView = new TextView(this);
+//			textView.setText(fileList.get(i).getName());
+//			textView.setPadding(5, 5, 5, 5);
+//
+//			System.out.println(fileList.get(i).getName());
+//
+////			if (fileList.get(i).isDirectory()) {
+////				textView.setTextColor(Color.parseColor("#FF0000"));
+////			}
+//
+//			if (fileList.get(i).isFile())
+//				view.addView(textView);
+//		}
+//
+//	}
+//
+//	public ArrayList<File> getfile(File dir) {
+//		File listFile[] = dir.listFiles();
+//		if (listFile != null && listFile.length > 0) {
+//			for (int i = 0; i < listFile.length; i++) {
+//
+//				if (listFile[i].isDirectory()) {
+//					fileList.add(listFile[i]);
+//					getfile(listFile[i]);
+//
+//				} else {
+////					if (listFile[i].getName().endsWith(".png")
+////							|| listFile[i].getName().endsWith(".jpg")
+////							|| listFile[i].getName().endsWith(".jpeg")
+////							|| listFile[i].getName().endsWith(".gif"))
+////
+////					{
+//						fileList.add(listFile[i]);
+////					}
+//				}
+//
+//			}
+//		}
+//		return fileList;
 	}
 
 }
