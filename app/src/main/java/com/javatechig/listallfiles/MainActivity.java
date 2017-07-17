79d5f82418075c57448cafc7a673991af7c69749
ji4 <bornofluv@gmail.com>
2017-07-17 11:32:33 +0800

    Add End Date

--- app/src/main/java/com/javatechig/listallfiles/MainActivity.java
+++ app/src/main/java/com/javatechig/listallfiles/MainActivity.java
@@ -26,25 +26,14 @@ public class MainActivity extends Activity {
     EditText et_fileName;
 	ScrollView scrollView;
 	LinearLayout ll;
-	EditText et_startYear, et_startMonth, et_startDay;
+	EditText et_startYear, et_startMonth, et_startDay, et_endYear, et_endMonth, et_endDay;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		resultView = (LinearLayout) findViewById(R.id.view);
-		scrollView = (ScrollView) findViewById(R.id.scrollView);
-		btn_search = (Button) findViewById(R.id.activity_main_btn_search);
-//		btn_clear = (Button) findViewById(R.id.activity_main_btn_clear);
-		btn_searchDate = (Button) findViewById(R.id.activity_main_btn_searchDate);
-		et_fileName = (EditText) findViewById(R.id.activity_main_et_fileName);
-		ll = (LinearLayout) findViewById(R.id.ll);
-		et_startYear = (EditText) findViewById(R.id.activity_main_et_startYear);
-		et_startMonth = (EditText) findViewById(R.id.activity_main_et_startMonth);
-		et_startDay = (EditText) findViewById(R.id.activity_main_et_startDay);
-
-
+		findAllViews();
 
 		btn_search.setOnClickListener(new Button.OnClickListener(){
 			@Override
@@ -57,46 +46,21 @@ public class MainActivity extends Activity {
 		btn_searchDate.setOnClickListener(new Button.OnClickListener(){
 			@Override
 			public void onClick(View view) {
-				int year = Integer.parseInt(et_startYear.getText().toString().trim());
-				int month = Integer.parseInt(et_startMonth.getText().toString().trim());
-				int day = Integer.parseInt(et_startDay.getText().toString().trim());
+				int startYear = Integer.parseInt(et_startYear.getText().toString().trim());
+				int startMonth = Integer.parseInt(et_startMonth.getText().toString().trim());
+				int startDay = Integer.parseInt(et_startDay.getText().toString().trim());
 
+				int endYear = Integer.parseInt(et_endYear.getText().toString().trim());
+				int endMonth = Integer.parseInt(et_endMonth.getText().toString().trim());
+				int endDay = Integer.parseInt(et_endDay.getText().toString().trim());
 
-				Date date = new Date();
-				Calendar calendar = Calendar.getInstance();
-				calendar.setTime(date);
-				calendar.set(Calendar.YEAR, year);
-				calendar.set(Calendar.MONTH, month - 1);
-				calendar.set(Calendar.DAY_OF_MONTH, day);
-				calendar.set(Calendar.HOUR, 0);
-				calendar.set(Calendar.MINUTE, 0);// for 0 min
-				calendar.set(Calendar.SECOND, 0);// for 0 sec
-				System.out.println("calendar.getTime(): "+calendar.getTime());// print 'Mon Mar 28 06:00:00 ALMT 2016'
-
-
+				Date startDate = setDate(startYear, startMonth, startDay, false);
+				Date endDate = setDate(endYear, endMonth, endDay, true);
 			}
 		});
-
-//		btn_clear.setOnClickListener(new Button.OnClickListener() {
-//			@Override
-//			public void onClick(View view) {
-//				resultView.post(new Runnable(){
-//					@Override
-//					public void run()
-//					{
-//						resultView.removeAllViews();
-//						resultView.invalidate();
-//						resultView.requestLayout();
-//					}
-//				});
-//
-//			}
-//		});
-
 	}
 
-
-    public void searchFiles(String strFileName){
+	public void searchFiles(String strFileName){
         //getting SDcard root path
 //        root = new File(Environment.getExternalStorageDirectory()
 //                .getAbsolutePath());
@@ -136,6 +100,39 @@ public class MainActivity extends Activity {
 			}
 		}
 		return fileList;
+	}
+
+	public Date setDate(int year, int month, int day, Boolean isEndDate){
+		Date date = new Date();
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, year);
+		calendar.set(Calendar.MONTH, month - 1);
+		if(isEndDate) day++;
+		calendar.set(Calendar.DAY_OF_MONTH, day);
+		calendar.set(Calendar.HOUR, 0);
+		calendar.set(Calendar.MINUTE, 0);// for 0 min
+		calendar.set(Calendar.SECOND, 0);// for 0 sec
+		System.out.println("calendar.getTime(): "+calendar.getTime());// print 'Mon Mar 28 06:00:00 ALMT 2016'
+
+		return date;
+	}
+
+	public void findAllViews(){
+		resultView = (LinearLayout) findViewById(R.id.view);
+		scrollView = (ScrollView) findViewById(R.id.scrollView);
+		btn_search = (Button) findViewById(R.id.activity_main_btn_search);
+//		btn_clear = (Button) findViewById(R.id.activity_main_btn_clear);
+		btn_searchDate = (Button) findViewById(R.id.activity_main_btn_searchDate);
+		et_fileName = (EditText) findViewById(R.id.activity_main_et_fileName);
+		ll = (LinearLayout) findViewById(R.id.ll);
+		//start date
+		et_startYear = (EditText) findViewById(R.id.activity_main_et_startYear);
+		et_startMonth = (EditText) findViewById(R.id.activity_main_et_startMonth);
+		et_startDay = (EditText) findViewById(R.id.activity_main_et_startDay);
+		//end date
+		et_endYear = (EditText) findViewById(R.id.activity_main_et_endYear);
+		et_endMonth = (EditText) findViewById(R.id.activity_main_et_endMonth);
+		et_endDay = (EditText) findViewById(R.id.activity_main_et_endDay);
 	}
 
 }
