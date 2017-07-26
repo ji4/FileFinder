bec5029af4a476013a5ad952f469721e151560d3
ji4 <bornofluv@gmail.com>
2017-07-26 12:21:30 +0800

    Add CallBack Interface

---
+++ app/src/main/java/com/javatechig/listallfiles/CallBack.java
@@ -0,0 +1,12 @@
+package com.javatechig.listallfiles;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Created by chiaying.wu on 2017/7/26.
+ */
+
+public interface CallBack {
+    public void receiveFiles(ArrayList<File> arrltFiles);
+}
