4d5d83cd018d864147eab7d09352bdd7e7cd894c
ji4 <ji4@users.noreply.github.com>
2017-08-09 11:06:27 +0800

    Add 'NewCallBack' Interface And Its Implementaion 'SharedFiles'

---
+++ app/src/main/java/com/javatechig/listallfiles/NewCallBack.java
@@ -0,0 +1,14 @@
+package com.javatechig.listallfiles;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Created by chiaying.wu on 2017/8/9.
+ */
+
+public interface NewCallBack {
+    void put(File fileProvided);
+    ArrayList<File> take();
+    Boolean isProviderFinished(Boolean isFininshed);
+}
---
+++ app/src/main/java/com/javatechig/listallfiles/SharedFiles.java
@@ -0,0 +1,29 @@
+package com.javatechig.listallfiles;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Created by chiaying.wu on 2017/8/9.
+ */
+
+public class SharedFiles implements NewCallBack {
+    private ArrayList<File> m_arrltFileFound = new ArrayList<File>();
+    private Boolean isProviderFinished = false;
+
+
+    @Override
+    public synchronized void put(File fileProvided) {
+        m_arrltFileFound.add(fileProvided);
+    }
+
+    @Override
+    public synchronized ArrayList<File> take() {
+        return m_arrltFileFound;
+    }
+
+    @Override
+    public Boolean isProviderFinished(Boolean isFininshed) {
+        return isProviderFinished;
+    }
+}
