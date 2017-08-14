cf6d70554aa0e203a4c02fbf8ca3237088b26a40
ji4 <ji4@users.noreply.github.com>
2017-08-14 12:18:34 +0800

    Add Search File Without Input

--- app/src/main/java/com/javatechig/listallfiles/Controller.java
+++ app/src/main/java/com/javatechig/listallfiles/Controller.java
@@ -13,29 +13,33 @@ public class Controller {
 
     public void SearchFilesByInput(Handler handler, List<String> strListInputText) {
         final CallBack forSearchAndFilter = new SharedFiles();
-
         Runnable done = new Runnable() {
             @Override
             public void run() {
                 forSearchAndFilter.setPutFileDone(true);
             }
         };
-
         CyclicBarrier barrier = new CyclicBarrier(2, done);
 
-        Runnable searchRunnable  = new FileSearcher(forSearchAndFilter, barrier);
-        Thread searchThread = new Thread(searchRunnable);
-        searchThread.start();
+//        Runnable secSearchRunnable  = new FileSearcher(forSearchAndFilter, barrier);
+//        Thread secSearchThread = new Thread(secSearchRunnable);
+//        secSearchThread.start();
 
-        Runnable secSearchRunnable  = new FileSearcher(forSearchAndFilter, barrier);
-        Thread secSearchThread = new Thread(secSearchRunnable);
-        secSearchThread.start();
+        Runnable searchRunnable;
 
         if (strListInputText != null) { //has input
-            Runnable filterRunnable  = new FileFilter(forSearchAndFilter, handler, strListInputText);
+            barrier = null; //set null if there is only one search thread
+            searchRunnable = new FileSearcher(forSearchAndFilter, barrier);
+
+            Runnable filterRunnable = new FileFilter(forSearchAndFilter, handler, strListInputText);
             Thread filterThread = new Thread(filterRunnable);
             filterThread.start();
+        } else {
+            searchRunnable = new FileSearcher(forSearchAndFilter, handler);
         }
+
+        Thread searchThread = new Thread(searchRunnable);
+        searchThread.start();
     }
 
 //    public void searchDupFiles(Handler handler){
